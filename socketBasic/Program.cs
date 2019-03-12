93eaaf3ea51b2b7d0b2ee512c3cf707bc2a3f731
carlesalonso <carlos.martinez@mataro.epiaedu.cat>
2019-03-12 18:55:38 +0100

    basic version

---
+++ socketBasic/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace socketBasic
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipaddr = IPAddress.Any;
+
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
+            try
+            { 
+
+            listenerSocket.Bind(ipep);
+
+            listenerSocket.Listen(1); // how many clients can connect
+
+            Console.WriteLine("Waiting for incoming connections");
+
+            var client = listenerSocket.Accept(); // Syncronous operation
+
+            Console.WriteLine("Client connected . "
+                + client.ToString() + " -IP End Point;"
+                + client.RemoteEndPoint.ToString());
+
+
+            var buffer = new byte[128];
+            int numberOfReceivedBytes = 0;
+            while (true)
+            {
+                numberOfReceivedBytes = client.Receive(buffer);
+
+                Console.WriteLine(numberOfReceivedBytes + " bytes received");
+
+                var receivedText = Encoding.ASCII.GetString(buffer, 0, numberOfReceivedBytes);
+                Console.WriteLine($"Data received: {receivedText}");
+
+                client.Send(buffer);
+                
+                Array.Clear(buffer, 0, buffer.Length);
+                numberOfReceivedBytes = 0;
+            }
+        }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+
+            Console.WriteLine("press a key to exit");
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ socketClient/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace socketClient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            var client = new Socket(AddressFamily.InterNetwork, 
+            SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipAddress;
+            int port;
+
+            try
+            {
+                Console.WriteLine("Client socket (press any key)");
+                Console.ReadKey();
+                Console.Write("Enter Server IP address: ");
+
+                string strIPAddress = Console.ReadLine();
+                Console.Write("Enter Server Port: ");
+                string strPort = Console.ReadLine();
+
+                if (!IPAddress.TryParse(strIPAddress, out ipAddress))
+                {
+                    Console.WriteLine("Invalid IP address");
+                    return;
+                }
+
+                if (!int.TryParse(strPort, out port))
+                {
+                    Console.WriteLine("Invalid port ");
+                    return;
+                }
+
+                Console.WriteLine($"IP Address: {ipAddress} - Port: {port}");
+                client.Connect(ipAddress, port);
+
+                Console.WriteLine("Connected to the server");
+                string inputText = "";
+                while (true)
+                {
+                    inputText = Console.ReadLine();
+                    if (inputText.Equals("EXIT"))
+
+                    {
+                        break;
+                    }
+
+                    var bufferSend = Encoding.ASCII.GetBytes(inputText);
+                    client.Send(bufferSend);
+
+                    var bufferReceived = new byte[128];
+                    int numberOfBytesReceived = client.Receive(bufferReceived);
+                    Console.WriteLine("Data received: {0}",
+                    Encoding.ASCII.GetString(bufferReceived, 0, numberOfBytesReceived));
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+
+            finally
+            {
+                if (client != null && client.Connected)
+                {
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                    client.Dispose();
+                }
+            }
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+    }
+}
